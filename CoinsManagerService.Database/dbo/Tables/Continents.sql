86f18c3b9784c5e4c970971e91e2a6f10ac1c2cc
Vadym Solodovnik <vad.solodovnik@gmail.com>
2023-04-19 09:35:10 +0200

    Add Database project and tables

---
+++ CoinsManagerService.Database/dbo/Tables/Continents.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Continents]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Continent] NVARCHAR(MAX) NOT NULL
+)
---
+++ CoinsManagerService.Database/dbo/Tables/Countries.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Countries]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Country] NVARCHAR(MAX) NOT NULL, 
+    [Continent] INT NOT NULL, 
+    CONSTRAINT [FK_Countries_Continents] FOREIGN KEY ([Continent]) REFERENCES [Continents]([ID])
+)
