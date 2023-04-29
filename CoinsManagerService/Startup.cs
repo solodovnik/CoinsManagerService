e7e9afa0186738e127eea317713264404b12bde3
Vadym Solodovnik <vad.solodovnik@gmail.com>
2023-04-29 14:03:57 +0200

    Add Azure AD authentication

--- CoinsManagerService/Startup.cs
+++ CoinsManagerService/Startup.cs
@@ -1,10 +1,12 @@
 using CoinsManagerService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
 using System;
 
 namespace CoinsManagerService
@@ -28,6 +30,7 @@ namespace CoinsManagerService
             {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Coins Manager", Version = "v1" });
             });
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +45,16 @@ namespace CoinsManagerService
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
+            app.Use(async (context, next) =>
+            {
+                if (!context.User.Identity?.IsAuthenticated ?? false)
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsync("Not Authenticated");
+                }
+                else await next();
+            });
 
             app.UseEndpoints(endpoints =>
             {
