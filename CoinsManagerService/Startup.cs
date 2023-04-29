535a322b54a49417c5a0ad2f7369b7e85c8edb26
Vadym Solodovnik <vad.solodovnik@gmail.com>
2023-04-29 20:11:16 +0200

    Add authentication to Swagger

--- CoinsManagerService/Startup.cs
+++ CoinsManagerService/Startup.cs
@@ -1,12 +1,12 @@
 using CoinsManagerService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 using System;
 
 namespace CoinsManagerService
@@ -19,7 +19,7 @@ namespace CoinsManagerService
         }
 
         public IConfiguration Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CoinsConn")));
@@ -29,6 +29,26 @@ namespace CoinsManagerService
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Coins Manager", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                 });
             });
             services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
         }
@@ -47,18 +67,6 @@ namespace CoinsManagerService
 
             app.UseAuthentication();
             app.UseAuthorization();
-            if (!env.IsDevelopment())
-            {
-                app.Use(async (context, next) =>
-                {
-                    if (!context.User.Identity?.IsAuthenticated ?? false)
-                    {
-                        context.Response.StatusCode = 401;
-                        await context.Response.WriteAsync("Not Authenticated");
-                    }
-                    else await next();
-                });
-            }
 
             app.UseEndpoints(endpoints =>
             {
