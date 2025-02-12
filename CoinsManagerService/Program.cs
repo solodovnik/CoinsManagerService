dcc30fe96c7e2fd2f1d85c209bff9eae4f92372a
Vadym Solodovnik <vad.solodovnik@gmail.com>
2025-02-12 10:36:00 +0100

    AB#384 Migrate to .NET 8 minimal hosting model

--- CoinsManagerService/Program.cs
+++ CoinsManagerService/Program.cs
@@ -1,20 +1,83 @@
-using Microsoft.AspNetCore.Hosting;
+using CoinsManagerService.Data;
+using CoinsManagerWebUI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using System;
 
-namespace CoinsManagerService
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("CoinsConn")));
+
+builder.Services.AddScoped<ICoinsRepo, CoinsRepo>();
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddSwaggerGen(c =>
 {
-    public static class Program
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Coins Manager", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n" +
+                      "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+                      "Example: \"Bearer 1safsfsdfdfd\"",
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {                    
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+builder.Services.AddSingleton<IAzureBlobService, AzureBlobService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Coins Manager API");
+});
+
+// Database Preparation
+PrepDb.PrepareDatabase(app);
+
+// Run the application
+app.Run();
